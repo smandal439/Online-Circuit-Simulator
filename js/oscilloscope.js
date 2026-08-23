58ac77cab03562febe1d0706aa9fc79788f32825
SUSHANTA <SUSHANTA@local>
2026-08-23 15:03:30 +0530

    fix: update color for CH1 and adjust fetch path for example sketches

--- js/oscilloscope.js
+++ js/oscilloscope.js
@@ -34,7 +34,8 @@ class Oscilloscope {
     };
 
     /* Colors */
-    this.CH1_COLOR = '#00e5ff';
+    this.CH1_COLOR = '#73ff00';
+    // this.CH1_COLOR = '#00e5ff';
     this.CH2_COLOR = '#ff9800';
     this.GRID_COLOR = 'rgba(255,255,255,0.06)';
     this.GRID_MAJOR = 'rgba(255,255,255,0.12)';
