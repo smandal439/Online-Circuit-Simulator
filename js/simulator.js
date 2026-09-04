3511cf14ebe96afaf4a2fabf10b309929946ebb5
Sushanta Mandal <154744591+smandal439@users.noreply.github.com>
2026-09-04 11:25:47 +0530

    Update simulator.js

--- js/simulator.js
+++ js/simulator.js
@@ -3111,7 +3111,7 @@ window.loadExamplesFromFiles = async function () {
     'esp32_fade', 'mqtt_esp32', 'lcd_i2c', 'oled_ssd1306', 'esp32_server', 'serial_plotter',
     'buzzer_melody', 'seg7_counter', 'relay_control', 'dc_motor_speed', 'stepper_motor',
     'neopixel_color_cycle', 'mpu6050_accel', 'ldr_lamp', 'pir_alarm', 'joystick_led',
-    'esp32_ntp_lcd', 'ic_nand_test', 'logic_analyzer_test', 'temperature_LCD', 'dmm_current',
+    'esp32_ntp_clock_lcd', 'ic_nand_test', 'logic_analyzer_test', 'temperature_LCD', 'dmm_current',
     'dmm_resistance', 'dmm_voltage', 'func_gen_dual', 'func_gen_led', 'remote_control_leds',
     'remote_servo_control', 'lm35_temperature', 'keypad_interfacing', 'bme280_weather',
     'bmp280_altitude', 'dso_oscilloscope', 'simplebme280_basic', 'simplebme280_altitude',
